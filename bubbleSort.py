38d66dd0361b1b1d2fc1eec4465e158ba583dc9e
Michael Champlin <mchamplin@sbcglobal.net>
2022-07-18 19:29:19 -0700

    Update bubbleSort.py

--- bubbleSort.py
+++ bubbleSort.py
@@ -1,1 +1,17 @@
-# write your bubble sort algorithm below
+# write your bubble sort algorithm belo
+
+def bubble_sort(lst):
+    for i in range(len(lst)-1):
+        for j in range(len(lst) - 1):
+
+            if (lst[j] > lst[j+1]):
+                temp = lst[j]
+                lst[j] = lst[j+1]
+                lst[j+1] = temp
+
+        print(lst)
+    return lst
+
+lst = [4,1,7,9,3,6]
+print(lst)
+print(bubble_sort(lst))
